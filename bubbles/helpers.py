9b3ef43c307be5be4f80f90b7118eae91e0306b9
Piotr Mamenas <piotr.mamenas@gmail.com>
2017-12-28 20:32:21 +0100

    Just adding correlation calc helpers as the one in stats package has rounding issues.

--- bubbles/helpers.py
+++ bubbles/helpers.py
@@ -1,2 +1,15 @@
 def get_answer():
     return True
+
+def de_mean(x):
+    xmean = mean(x)
+    return [xi - xmean for xi in x]
+
+def covariance(x,y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n-1)
+
+def correlation(x,y):
+    stddev_x = x.std()
+    stddev_y = y.std()
+    return covariance(x,y) / stddev_x / stddev_y
