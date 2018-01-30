789fe2c3c1c7875a2448ccb8b7926ba6494c6441
Piotr Mamenas <piotr.mamenas@gmail.com>
2018-01-30 23:18:20 +0100

    adding non naive bayes

---
+++ learning/nnb.py
@@ -0,0 +1,52 @@
+import numpy as np
+from loader import Loader as ld
+from datetime import datetime
+from scipy.stats import norm
+from scipy.stats import multivariate_normal as mvn
+
+class NNB(object):
+    def fit(self, X, Y, smoothing=10e-3):
+        N, D = X.shape
+        self.gaussians = dict()
+        self.priors = dict()
+        labels = set(Y)
+        for c in labels:
+            current_x = X[Y == c]
+            self.gaussians[c] = {
+                'mean': current_x.mean(axis=0),
+                'var': np.cov(current_x.T) + np.eye(D)*smoothing,
+            }
+            self.priors[c] = float(len(Y[Y == c])) / len(Y)
+
+    def score(self, X, Y):
+        P = self.predict(X)
+        return np.mean(P == Y)
+
+    def predict(self, X):
+        N, D = X.shape
+        K = len(self.gaussians)
+        P = np.zeros((N, K))
+        for c, g in self.gaussians.items():
+            mean, var = g['mean'], g['var']
+            P[:,c] = mvn.logpdf(X, mean=mean, cov=var) + np.log(self.priors[c])
+        return np.argmax(P, axis=1)
+
+
+if __name__ == '__main__':
+    X, Y = ld.load_mnist_train(10000);
+    Ntrain = 5000
+    Xtrain, Ytrain = X[:Ntrain], Y[:Ntrain]
+    Xtest, Ytest = X[Ntrain:], Y[Ntrain:]
+
+    model = NaiveBayes()
+    t0 = datetime.now()
+    model.fit(Xtrain, Ytrain)
+    print("Training time:", (datetime.now() - t0))
+
+    t0 = datetime.now()
+    print("Train accuracy:", model.score(Xtrain, Ytrain))
+    print("Time to compute train accuracy:", (datetime.now() - t0), "Train size:", len(Ytrain))
+
+    t0 = datetime.now()
+    print("Train accuracy:", model.score(Xtest, Ytest))
+    print("Time to compute train accuracy:", (datetime.now() - t0), "Train size:", len(Ytest))
