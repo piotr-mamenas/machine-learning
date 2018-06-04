10b74aa2cfd3a5692281b68c86cecbc21bc7156c
Piotr <piotr.mamenas@gmail.com>
2018-06-04 23:53:42 +0200

    dataset exploration, also finishing the buildup for the ann in tensorflow.

--- learning/main.py
+++ learning/main.py
@@ -3,20 +3,41 @@ import matplotlib.pyplot as plt
 
 from loader import Loader as ld
 from train_test_split import TrainTestSplit as tts
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-ld.fetch_housing_data()
-housing = ld.load_housing_data()
+def discovery():
+    ld.fetch_housing_data()
+    housing = ld.load_housing_data()
 
-housing.info()
-housing.hist(bins=50, figsize=(20,15))
+    housing.info()
+    housing.hist(bins=50, figsize=(20,15))
 
-plt.show()
+    plt.show()
 
-housing_with_id = housing.reset_index()
-housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
-train_set, test_set = tts.split_train_test_by_id(housing_with_id, 0.2, "id")
+    housing_with_id = housing.reset_index()
+    housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+    train_set, test_set = tts.split_train_test_by_id(housing_with_id, 0.2, "id")
 
-train_set.info()
+    train_set.info()
 
-X = ld.load_mnist_train()
-print(X)
+    X = ld.load_mnist_train()
+    print(X)
+    
+if __name__ == '__main__':    
+    lbc = load_breast_cancer()
+    
+    print(lbc)
+    print(lbc.data)
+    print(lbc.target)
+    
+    x = lbc.data
+    y = lbc.target
+    
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=44)
+    
+    rf = RandomForestClassifier()
+    rf.fit(x_train,y_train)
+    score = rf.score(x_test,y_test)
+    print(score)
--- learning/tensorflow-playground.py
+++ learning/tensorflow-playground.py
@@ -6,12 +6,18 @@ import pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.utils import shuffle
 
+class AnnHelper(object):
+    def get_weight_and_bias(self, input_size, output_size):
+        W = np.random.randn(input_size, output_size) / np.sqrt(input_size + output_size)
+        b = np.zeros(output_size)
+        return W.astype(np.float32), b.astype(np.float32)
+
 class HiddenLayer(object):
     def __init__(self, input_size, output_size, an_id):
         self.id = an_id
         self.input_size = input_size
         self.output_size = output_size
-        W, b = get_weight_and_bias(input_size, output_size)
+        W, b = AnnHelper.get_weight_and_bias(input_size, output_size)
         self.W = tf.Variable(W.astype(np.float32))
         self.b = tf.Variable(b.astype(np.float32))
         self.params = [self.W, self.B]
@@ -19,14 +25,9 @@ class HiddenLayer(object):
     def forward(self, X):
         return tf.nn.relu(tf.matmul(X, self.W) + self.b)
 
-    def get_weight_and_bias(self, input_size, output_size):
-        W = np.random.randn(input_size, output_size) / np.sqrt(input_size + output_size)
-        b = np.zeros(output_size)
-        return W.astype(np.float32), b.astype(np.float32)
-
 class ANN(object):
-    def __init__(self, hidden_layer_sizes):
-        self.hidden_layer_sizes = hidden_layer_sizes
+    def __init__(self, hidden_layers_number):
+        self.hidden_layers_number = hidden_layers_number
 
     def fit(self, data, targets, learning_rate=10e-7, mu=0.99, decay=0.999, reg=10e-3, epochs=400, batch_sz=100, show_fig=false):
         target_lenght = len(set(targets))
@@ -34,6 +35,72 @@ class ANN(object):
         data, targets = shuffle(data, targets)
         data = data.astype(np.float32)
         targets = hotcode_targets(targets).astype(np.float32)
+
+        x_train, x_test, y_train, y_test = train_test_split(data ,targets, test_size=0.2, random_state=44)
+        y_test_flat = np.argmax(y_test, axis=1)
+
+        n, d = x_train.shape
+        self.hidden_layers = []
+        count = 0
+        input_index = d
+
+        for output_index in self.hidden_layer_number:
+            layer = HiddenLayer(input_index, output_index, count)
+            self.hidden_layers.append(h)
+            input_index = output_index
+            count += 1
+        W, b = AnnHelper.get_weight_and_bias(input_index, target_lenght)
+        self.W = tf.Variable(W.astype(np.float32))
+        self.b = tf.Variable(b.astype(np.float32))
+
+        self.params = [self.W, self.b]
+        for h in self.hidden_layers:
+            self.params += h.params
+
+        tfX = tf.placeholder(tf.float32, shape=(None, d), name='tfX')
+        tfY = tf.placeholder(tf.float32, shape=(None, target_lenght), name='tfY')
+        activation = self.forward(tfX)
+
+        reg_cost = reg*sum([tf.nn.l2_loss(p) for p in self.params])
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(activation, tfY)) + reg_cost
+        prediction = self.predict(tfX)
+        train_operation = tf.train.RMSPropOptimizer(learning_rate,decay=decay, momentum=mu).minimize(cost)
+
+        n_batches = n / batch_sz
+        costs = []
+        init = tf.initialize_all_variables()
+        with tf.Session() as session:
+            session.run(init)
+            for i in range(epochs):
+                x_train, y_train = shuffle(x_train, y_train)
+                for j in range(n_batches):
+                    x_batch = x_train[j*batch_sz:(j*batch_sz + batch_sz)]
+                    y_batch = y_train[j*batch_sz:(j*batch_sz + batch_sz)]
+
+                    session.run(train_operation, feed_dict={tfX: x_batch, tfY: y_batch})
+
+                    if j % 20 == 0:
+                        c = session.run(cost, feed_dict={tfX: x_test, tfY: y_test})
+                        costs.append(c)
+
+                        p = session.run(prediction, feed_dict={tfX: x_test, tfY: y_test})
+                        e = error_rate(y_test_flat, p)
+
+                        print('i:',i,'j:',j,'batches:',n_batches,'cost:',c,'error:',e)
+
+        if show_fig:
+            plt.plot(costs)
+            plt.show()
+
+    def forward(self, x):
+        z = x
+        for h in self.hidden_layers:
+            z = h.forward(z)
+        return tf.matmul(z, self.W) + self.b;
+
+    def predict(self, x):
+        activation = self.forward(x)
+        return tf.argmax(activation, 1)
 
     def hotcode_targets(targets):
         size_x = len(targets)
@@ -45,6 +112,10 @@ class ANN(object):
 
 class Playground(object):
     def main(self):
-        ANN = ANN(3)
+        breast_cancer_data = load_breast_cancer()
+        model = ANN([500, 250, 125])
+        model.fit(breast_cancer_data.data, breast_cancer_data.target, show_fig=True)
 
 if __name__ == '__main__':
+    playground = Playground()
+    playground.main()
